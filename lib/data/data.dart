61965bce6eb0ac96c54ca5d9b9803e398af3386e
Nakatani <46980918+Khanno@users.noreply.github.com>
2021-11-17 16:52:16 -0300

    feat(data): feat mocked data (#2)

---
+++ lib/data/data.dart
@@ -0,0 +1,80 @@
+List<Map<String, dynamic>> data = [
+  {
+    'name': 'Apple Fuji',
+    'value': 129,
+    'description': 'Fuji apples are typically round and range from large to very large, averaging 75 mm in diameter',
+    'image': 'assets/images/apple_fuji.jpg',
+  },
+  {
+    'name': 'Apple Gala',
+    'value': 160,
+    'description': 'Gala apples are non-uniform in colour, usually vertically striped or mottled, with overall orange color.',
+    'image': 'assets/images/apple_gala.jpg',
+  },
+  {
+    'name': 'Apple Honeycrisp',
+    'value': 145,
+    'description': 'Its sweetness, firmness, and tartness make it an ideal apple for eating raw.',
+    'image': 'assets/images/apple_honeycrisp.jpg',
+  },
+  {
+    'name': 'Asian Pear',
+    'value': 210,
+    'description': 'The fruit tends to be quite large and fragrant, and when carefully wrapped (it has a tendency to bruise because of its juiciness)',
+    'image': 'assets/images/asian_pear.jpg',
+  },
+  {
+    'name': 'European Pear',
+    'value': 280,
+    'description': 'It is one of the most important fruits of temperate regions, being the species from which most orchard pear cultivars grown in Europe, North America, and Australia have been developed',
+    'image': 'assets/images/european_pear.jpg',
+  },
+  {
+    'name': 'Yellow Pear',
+    'value': 270,
+    'description': 'Like most pears, its fruit can be eaten raw or cooked; it has a mild sour taste.',
+    'image': 'assets/images/yellow_pear.jpg',
+  },
+  {
+    'name': 'Cavendish Banana',
+    'value': 310,
+    'description': 'The fruits of the Cavendish bananas are eaten raw, used in baking, fruit salads, and to complement foods.',
+    'image': 'assets/images/cavendish_banana.jpg'
+  },
+  {
+    'name': 'Scarlet Banana',
+    'value': 330,
+    'description': 'commonly known as scarlet banana or red-flowering banana, is a species of flowering plant in the banana and plantain family Musaceae, native to tropical China and Vietnam',
+    'image': 'assets/images/scarlet_banana.jpg',
+  },
+  {
+    'name': 'Apple Banana',
+    'value': 320,
+    'description': 'Latundan bananas are popular dessert bananas. They are also cultivated as ornamental plants.',
+    'image': 'assets/images/apple_banana.jpg',
+  },
+  {
+    'name': 'Queen Pineapple',
+    'value': 295,
+    'description': 'Queen Pineapple Fruits are Spiny, golden yellow in color and emits pleasant aroma and flavor at the ripen stage',
+    'image': 'assets/images/queen_pineapple.jpg',
+  },
+  {
+    'name': 'Smooth Cayenne Pineapple',
+    'value': 315,
+    'description': 'Yellow-fleshed, cylindrical fruit with high sugar and acid content, is well-adapted to canning and processing; its leaves are without spines',
+    'image': 'assets/images/smooth_cayanne.jpg',
+  },
+  {
+    'name': 'Keitt Mango',
+    'value': 410,
+    'description': 'The flesh has fibers, tangy and sweet, with a monoembryonic seed',
+    'image': 'assets/images/keitt_mango.jpg',
+  },
+  {
+    'name': 'Irwin mango',
+    'value': 450,
+    'description':  'The flesh is yellow and has a mild but sweet flavor and a pleasant aroma. It is fiberless and contains a monoembryonic seed',
+    'image': 'assets/images/irwin_mango.jpg',
+  }
+];
---
+++ lib/model/product_model.dart
@@ -0,0 +1,8 @@
+class Product {
+  String name;
+  String description;
+  int value;
+  String image;
+
+  Product({required this.name, required this.description, required this.value, required this.image});
+}
